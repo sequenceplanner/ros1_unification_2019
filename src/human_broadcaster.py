ddddf7c8c78d1f4952064ee929ae524bd877f9ec
endre90 <endre90@gmail.com>
2019-05-24 20:55:24 +0200

    progress with the kinect

--- src/human_broadcaster.py
+++ src/human_broadcaster.py
@@ -4,7 +4,7 @@
 # authors, description, version
 #----------------------------------------------------------------------------------------
     # Endre Eres
-    # Kinect Human tf Broadcaster
+    # Kinect Humans tf Broadcaster
     # V.0.0.1.
 #----------------------------------------------------------------------------------------
 
@@ -14,9 +14,6 @@ import time
 import rospy
 import roslib
 from ros1_unification_2019.msg import KinectHumans
-from ros1_unification_2019.msg import JointPosition
-from ros1_unification_2019.msg import HumanJoints
-
 
 class kinect_human_broadcaster():
 
@@ -30,35 +27,13 @@ class kinect_human_broadcaster():
         self.br = tf.TransformBroadcaster()
         self.human_list = []
         self.joint_list = []
+        self.distances = []
         self.x = 0
 
         self.human_id_list = [1, 2, 3, 4, 5, 6]
 
-        self.joint_name_list = ["SpineBase",
-                          "SpineMid",
-			              "Neck",
-			              "Head",
-			              "ShoulderLeft",
-			              "ElbowLeft",
-			              "WristLeft",
-			              "HandLeft",
-			              "ShoulderRight",
-			              "ElbowRight",
-			              "WristRight",
-			              "HandRight",
-			              "HipLeft",
-			              "KneeLeft",
-			              "AnkleLeft",
-			              "FootLeft",
-			              "HipRight",
-			              "KneeRight",
-			              "AnkleRight",
-			              "FootRight",
-			              "SpineShoulder",
-			              "HandTipLeft",
-			              "ThumbLeft",
-			              "HandTipRight",
-                          "ThumbRight" ]
+        # Filtered out the non-essential joints
+        self.joint_name_list = ["Neck", "Head", "ShoulderLeft", "ShoulderRight", "SpineShoulder"]
 
         self.rate = rospy.Rate(25)
         self.main()
@@ -73,6 +48,8 @@ class kinect_human_broadcaster():
 
         while not rospy.is_shutdown():
 
+            self.distances = []
+
             if self.human_list != []:
                 self.human_list_sample = self.human_list
 
@@ -82,19 +59,34 @@ class kinect_human_broadcaster():
                     self.joint_list = self.human_list_sample[j].joint_list
 
                     for i in range(0, len(self.joint_list)):
-                    
-                        print(self.joint_list[i])
 
-                        self.br.sendTransform((self.joint_list[i].x, self.joint_list[i].y, self.joint_list[i].z),
+                        if self.joint_list[i].joint_id in self.joint_name_list:
+
+                            self.br.sendTransform((self.joint_list[i].x, self.joint_list[i].y, self.joint_list[i].z),
                                               (0, 0, 0, 1),
                                               rospy.Time.now(),
                                               str(self.joint_list[i].joint_id) + str(self.human_id),
                                               "kinect")
-                
-            #self.rate.sleep()
 
-            else:
-                pass
+                            if self.joint_list[i].z > 1 and self.joint_list[i].z < 2:
+                                self.distances.append("close")
+                            elif self.joint_list[i].z > 2 and self.joint_list[i].z < 3:
+                                self.distances.append("far")
+                            else:
+                                self.distances.append("unknown")
+
+                        else:
+                            pass
+
+                    if "close" in self.distances:
+                        print("human " + str(self.human_id) + " is close")
+                    elif "close" not in self.distances and "far" in self.distances:
+                        print("human " + str(self.human_id) + " is far")
+                    else:
+                        print("human " + str(self.human_id) + " is unknown")                   
+                            
+                else:
+                    pass
                 
             self.rate.sleep()
         rospy.spin()
